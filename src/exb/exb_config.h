289a838b5c56876af9bd1a54b3a4732dd9fe060e
turki alsaleem <nilputs@gmail.com>
2020-04-22 21:10:54 +0300

    fix config

--- src/exb/exb_config.h
+++ src/exb/exb_config.h
@@ -1,23 +1,23 @@
-#ifndef CPB_CONFIG_H
-#define CPB_CONFIG_H
+#ifndef EXB_CONFIG_H
+#define EXB_CONFIG_H
 
 /*Eloop env config*/
-#define CPB_MAX_ELOOPS 128
+#define EXB_MAX_ELOOPS 128
 /*End Eloop env config*/
 
 /*Server configuration*/
 #define LISTEN_BACKLOG 8000
-#define CPB_SOCKET_MAX 8192
-#define CPB_SERVER_MAX_MODULES 11
-#define CPB_HTTP_MIN_DELAY 0//ms
+#define EXB_SOCKET_MAX 8192
+#define EXB_SERVER_MAX_MODULES 11
+#define EXB_HTTP_MIN_DELAY 0//ms
 
-#define CPB_USE_READ_WRITE_FOR_TCP
-#undef  CPB_USE_READ_WRITE_FOR_TCP
+#define EXB_USE_READ_WRITE_FOR_TCP
+#undef  EXB_USE_READ_WRITE_FOR_TCP
 
 /*End Server configuration*/
 /*Pcontrol config*/
-#define CPB_MAX_PROCESSES 128
-#define CPB_MAX_HOOKS 16
+#define EXB_MAX_PROCESSES 128
+#define EXB_MAX_HOOKS 16
 /*End Pcontrol config*/
 
 #endif
